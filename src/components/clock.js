4ee1080e7cdef89dc7f134ed4d475cfcd48d6aaf
Jason Kuhrt <jasonkuhrt@me.com>
2016-06-06 11:57:18 -0400

    Align/Rotate all hands.

--- src/components/clock.js
+++ src/components/clock.js
@@ -10,24 +10,39 @@ export default class Clock extends React.Component {
       minutes,
       seconds,
     } = this.props
-    
-    // get a percent (0-1) from hours
-    // get a degrees from that percent
+
+
+    const absoluteStyle = {
+      position: "absolute",
+      top: 0,
+      left: 0,
+    }
+
     const hourDegrees = (hours / 12) * 360
     const minDegrees = (minutes / 60) * 360
     const secDegrees = (seconds / 60) * 360
+  
+    // TODO Fix alignment on hands with width greater than 1px.
 
     const faceStyle = {
       position: "relative",
       width: 500,
-      height: 500,      
+      height: 500,
       backgroundColor: "#222",
       borderRadius: "50%"
     }
+    const pinStyle = {
+      ...absoluteStyle,
+      width: 1,
+      height: 1,
+      borderRadius: "50%",
+      backgroundColor: "red",
+      transform: "translate(0px, 0px)",
+      top: "50%",
+      left: "50%",
+    }
     const hourStyle = {
-      position: "absolute",
-      top: 0,
-      left: 0,
+      ...absoluteStyle,
       width: 20,
       height: 100,
       transformOrigin: "50% 100%",
@@ -35,21 +50,19 @@ export default class Clock extends React.Component {
       backgroundColor: "#eee",
     }
     const minStyle = {
-      position: "absolute",
-      top: 0,
-      left: 0,
+      ...absoluteStyle,
       width: 10,
       height: 240,
       transformOrigin: "50% 100%",
-      transform: `translate(250px, 0px) rotate(${minDegrees}deg)`,
+      transform: `translate(250px, 10px) rotate(${minDegrees}deg)`,
       backgroundColor: "#eee",
     }
     const secStyle = {
-      position: "absolute",
-      width: 20,
-      height: 100,
+      ...absoluteStyle,
+      width: 1,
+      height: 250,
       transformOrigin: "50% 100%",
-      transform: `translate(250px, 150px) rotate(${hourDegrees}deg)`,
+      transform: `translate(250px, 0px) rotate(${secDegrees}deg)`,
       backgroundColor: "#eee",
     }
     return (
@@ -59,6 +72,7 @@ export default class Clock extends React.Component {
         <div style={hourStyle}>{hours}</div>
         <div style={minStyle}>{minutes}</div>
         <div style={secStyle}>{seconds}</div>
+        <div style={pinStyle}></div>
       </div>
     )
   }
