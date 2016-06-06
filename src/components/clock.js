77800369acc3d7617bb69fa7e8cb51fb05fd543f
James Paterson <j@presstube.com>
2016-06-06 11:28:06 -0400

    stubbing hands, and fleshing out hour hand

--- src/components/clock.js
+++ src/components/clock.js
@@ -10,11 +10,37 @@ export default class Clock extends React.Component {
       minutes,
       seconds,
     } = this.props
+    
+    const hourDegrees = 90
+    const minDegrees = 180
+    const secDegrees = 270
+
+    const faceStyle = {
+      width: 500,
+      height: 500,      
+      backgroundColor: "#222",
+      borderRadius: "50%"
+    }
+    const hourStyle = {
+      width: 10,
+      height: 250,
+      transformOrigin: "50% 100%",
+      transform: "translate(250px, 0) rotate(77deg)",
+      backgroundColor: "#eee",
+    }
+    const minStyle = {
+
+    }
+    const secStyle = {
+
+    }
     return (
-      <div>
-        <div>{hours}</div>
-        <div>{minutes}</div>
-        <div>{seconds}</div>
+      // container div that's a black circle 500 diameter
+      // nested inside 3 divs: hr, min, sec
+      <div style={faceStyle}>
+        <div style={hourStyle}>{hours}</div>
+        <div style={minStyle}>{minutes}</div>
+        <div style={secStyle}>{seconds}</div>
       </div>
     )
   }
